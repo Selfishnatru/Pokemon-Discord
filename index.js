26cfd9d48f739daa59d70a2914cdefabfc374d13
Selfishnatru <70726867+Selfishnatru@users.noreply.github.com>
2020-09-03 15:13:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+var PORT = process.env.PORT || 5000;
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const client = new Discord.Client();
 
+client.once('ready', () => {
+    console.log('Ready')
+    client.user.setActivity('Eating Your Family', { type: "WATCHING"}).catch(console.error);
+})
+client.login(token);
+
+
+client.on('message', message => {
+    console.log(message.content);
+})
+
+client.on('message', msg=>{
+    if(msg.content == "test"){
+        msg.reply('test1');
+    }
+})
